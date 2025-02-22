6188230c12f00306457a510cb6a92511c8f5f4f3
ULTHI MANJUNATH <141410221+ulthimanju@users.noreply.github.com>
2025-02-22 12:32:25 +0530

    Update arduino_serial_control.ino

--- arduino_serial_control.ino
+++ arduino_serial_control.ino
@@ -68,9 +68,6 @@ int32_t                width  = 0, // BMP image dimensions
 ImageReturnCode stat;
 int st=0;
 void setup(void) {
-
-  
-
   Serial.begin(9600);
   while(!Serial)  delay(100);       // Wait for Serial Monitor before continuing
 
@@ -100,571 +97,159 @@ void setup(void) {
 #endif
   Serial.println(F("OK!"));
 
- 
   tft.fillScreen(ILI9341_BLUE);
-
- 
-
-
-  
   delay(2000); // Pause 2 seconds before moving on to loop()
 }
 
 void loop() {
-
   if(st==0)
   {
     st=1;
-  
-  Serial.print(F("Loading purple.bmp to screen..."));
-  stat = reader.drawBMP("/1.bmp", tft, 0, 0);
-  reader.printStatus(stat);   // How'd we do?
-    delay(3000); // Pause 2 sec.
-}
+    Serial.print(F("Loading purple.bmp to screen..."));
+    stat = reader.drawBMP("/welcome.bmp", tft, 0, 0);
+    reader.printStatus(stat);   // How'd we do?
+    delay(3000); 
+    st=2;
+    Serial.print(F("Loading purple.bmp to screen..."));
+    stat = reader.drawBMP("/1.bmp", tft, 0, 0);
+    reader.printStatus(stat);   // How'd we do?
+    delay(3000); 
+  }
 
   if(Serial.available())
   {
-    
-  int x=Serial.read();
-  if(x=='1')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_RED);
-    delay(2000); 
-    stat = reader.drawBMP("/tq.bmp", tft, 0, 0);
-    reader.printStatus(stat);   // How'd we do?
-    delay(6000); // Pause 2 sec.
+    int x=Serial.read();
+    if(x=='1')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_RED);
+      delay(2000); 
+      stat = reader.drawBMP("/tq.bmp", tft, 0, 0);
+      reader.printStatus(stat);
+      delay(6000);
+      st=0;
+    }
+    else if(x=='2')
+    {
+      st=2;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000); 
+      stat = reader.drawBMP("/gb.bmp", tft, 0, 0);
+      reader.printStatus(stat);
+      delay(6000); // Pause 2 sec.
+      st=0;
+    }
+    else if(x=='3')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(200);
+      stat = reader.drawBMP("/how.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(6000);
+      st=2;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(200);
+      stat = reader.drawBMP("/you.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(6000);
+      st=0;
+    }
+    else if(x=='4')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000);
+      stat = reader.drawBMP("/where.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(2000);
+      st=2;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000);
+      stat = reader.drawBMP("/you.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(2000);
+      st=3;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000);
+      stat = reader.drawBMP("/from.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(2000);
+      st=0;
+    }
+    else if(x=='5')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(200);
+      stat = reader.drawBMP("/drink.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(6000);
+      st=2;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000);
+      stat = reader.drawBMP("/water.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(2000);
+      st=0;
+    }
+    else if(x=='6')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000); 
+      stat = reader.drawBMP("/i am.bmp", tft, 0, 0);
+      reader.printStatus(stat);   // How'd we do?
+      delay(6000);
+      st=2;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000); 
+      stat = reader.drawBMP("/fine.bmp", tft, 0, 0);
+      reader.printStatus(stat);   // How'd we do?
+      delay(6000);
+      st = 0;
+    }
+    else if(x=='7')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(2000); 
+      stat = reader.drawBMP("/lunch.bmp", tft, 0, 0);
+      reader.printStatus(stat);   // How'd we do?
+      delay(6000); // Pause 2 sec.
+      st = 0;
+    }
+    else if(x=='8')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(200);
+      stat = reader.drawBMP("/welcome.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(6000);
+      st = 0;
+    }
+    else if(x=='9')
+    {
+      st=1;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(200);
+      stat = reader.drawBMP("/we.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(6000);
+      st=2;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(200);
+      stat = reader.drawBMP("/from.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(6000);
+      st=3;
+      tft.fillScreen(ILI9341_BLUE);
+      delay(200);
+      stat = reader.drawBMP("/viat ap.bmp", tft, 0,0);
+      reader.printStatus(stat);
+      delay(6000);
+      st = 0;
+    }
   }
-  if(x=='2')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(2000); 
-    stat = reader.drawBMP("/gb.bmp", tft, 0, 0);
-    reader.printStatus(stat);
-    delay(6000); // Pause 2 sec.
-  }
-  if(x=='3')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/angry.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='4')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/aunt.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='5')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/baby.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-    if(x=='6')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(2000); 
-    stat = reader.drawBMP("/bag.bmp", tft, 0, 0);
-    reader.printStatus(stat);   // How'd we do?
-    delay(6000); // Pause 2 sec.
-  }
-  if(x=='7')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(2000); 
-    stat = reader.drawBMP("/bathroom.bmp", tft, 0, 0);
-    reader.printStatus(stat);   // How'd we do?
-    delay(6000); // Pause 2 sec.
-  }
-  if(x=='8')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/bed.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='9')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/book.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='10')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/bored.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-    if(x=='11')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(2000); 
-    stat = reader.drawBMP("/bro.bmp", tft, 0, 0);
-    reader.printStatus(stat);   // How'd we do?
-    delay(6000); // Pause 2 sec.
-  }
-  if(x=='12')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(2000); 
-    stat = reader.drawBMP("/brother.bmp", tft, 0, 0);
-    reader.printStatus(stat);   // How'd we do?
-    delay(6000); // Pause 2 sec.
-  }
-  if(x=='13')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/car.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='14')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/chair.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='15')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/clock.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-    if(x=='16')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(2000); 
-    stat = reader.drawBMP("/clothes.bmp", tft, 0, 0);
-    reader.printStatus(stat);   // How'd we do?
-    delay(6000); // Pause 2 sec.
-  }
-  if(x=='17')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(2000); 
-    stat = reader.drawBMP("/cold.bmp", tft, 0, 0);
-    reader.printStatus(stat);   // How'd we do?
-    delay(6000); // Pause 2 sec.
-  }
-  if(x=='18')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/color.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='19')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/computer.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if(x=='20')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/confused.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='21')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/cousin.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='22')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/curious.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-    if (x=='23')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/dad.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='24')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/daughter.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='25')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/day.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='26')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/dont know.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='27')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/door.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='28')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/download.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='29')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/drink.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='30')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/early.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='31')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/earth.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='32')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/eat.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='33')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/embarrassed.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='34')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/evening.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='35')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/false.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='36')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/father.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='37')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/finish.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='38')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/food.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='39')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/fork.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='40')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/friend.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='41')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/frustrated.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='42')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/future.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='43')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/gift.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='44')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/glasses.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='45')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/good morning.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='46')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/good night.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='47')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/goodbye.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='48')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/grandfather.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='49')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/grandmother.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='50')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/happy.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='51')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/hat.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='52')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/hello.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='53')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/help.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='54')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/hi.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='55')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/home.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='56')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/hot.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='57')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/hour.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='58')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/how many.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='59')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/how much.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-  if (x=='60')
-  {
-    st=0;
-     tft.fillScreen(ILI9341_BLUE);
-    delay(200);
-    stat = reader.drawBMP("/how.bmp", tft, 0,0);
-    reader.printStatus(stat);
-    delay(6000);
-  }
-}
 }
